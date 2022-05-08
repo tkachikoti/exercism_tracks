0b4d0de71b39768b81781e88995740e7a92ee776
Tinashe Kachikoti <tinashekachikoti@gmail.com>
2022-05-08 22:59:23 +0100

    complete second iteration

--- javascript/annalyns-infiltration/annalyns-infiltration.js
+++ javascript/annalyns-infiltration/annalyns-infiltration.js
@@ -8,7 +8,7 @@
  * @return {boolean} Whether or not you can execute a fast attack.
  */
 export function canExecuteFastAttack(knightIsAwake) {
-  return (!knightIsAwake) ? true : false;
+  return !knightIsAwake;
 }
 
 /**
@@ -21,7 +21,7 @@ export function canExecuteFastAttack(knightIsAwake) {
  * @returns {boolean} Whether or not you can spy on someone.
  */
 export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
-  return (knightIsAwake || archerIsAwake || prisonerIsAwake) ? true : false;
+  return knightIsAwake || archerIsAwake || prisonerIsAwake;
 }
 
 /**
@@ -33,7 +33,7 @@ export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
  * @returns {boolean} Whether or not you can send a signal to the prisoner.
  */
 export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
-  return (!archerIsAwake && prisonerIsAwake) ? true : false;
+  return !archerIsAwake && prisonerIsAwake;
 }
 
 /**
@@ -52,17 +52,8 @@ export function canFreePrisoner(
   prisonerIsAwake,
   petDogIsPresent
 ) {
-  if (petDogIsPresent) {
-    if (!archerIsAwake) {
-      return true;
-    } else {
-      return false;
-    }
-  } else {
-    if (prisonerIsAwake && !knightIsAwake && !archerIsAwake) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+  return (
+    (petDogIsPresent && !archerIsAwake) || 
+    (prisonerIsAwake && !knightIsAwake && !archerIsAwake)
+  );
 }
